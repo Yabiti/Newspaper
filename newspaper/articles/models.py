96d4fb52a6e229a244653fdf08be6a4eff56738c
Yabiti <yabetseyohannes@gmail.com>
2025-01-31 03:11:35 -0800

    .

--- newspaper/articles/models.py
+++ newspaper/articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete= models.CASCADE()
+    )
