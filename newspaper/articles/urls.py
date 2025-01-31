34170c7c118dc5ad1dd314479371d5ec3e2072ea
Yabiti <yabetseyohannes@gmail.com>
2025-01-31 04:07:57 -0800

    .

--- newspaper/articles/urls.py
+++ newspaper/articles/urls.py
@@ -4,7 +4,7 @@ from .views import ArticleListView, DetailListView, DeleteListView, UpdateListVi
 
 urlpatterns = [
     path("<int:pk>/edit", UpdateListView.as_view(), name="article_edit"),
-    path("<int:pk>/", DetailListView.as_view, name="article_detail"),
+    path("<int:pk>/", DetailListView.as_view(), name="article_detail"),
     path("<int:pk>/delete/", DeleteListView.as_view(), name="article_delete"),
-    path("", ArticleListView.as_view(), name="article_list")
+    path("", ArticleListView.as_view(), name="list")
 ]
--- newspaper/articles/views.py
+++ newspaper/articles/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 
 class ArticleListView(ListView):
     model = Article
-    template_name = "article_list.html"
+    template_name = "articles/home.html"
 
 class  DetailListView(DetailView):
     model = Article
