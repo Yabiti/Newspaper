22a93e74f13c94fb5cebb840ea0d70295deede0b
Yabiti <yabetseyohannes@gmail.com>
2025-01-31 03:29:28 -0800

    .

---
+++ newspaper/articles/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import ArticleListView, DetailListView, DeleteListView, UpdateListView
+
+urlpatterns = [
+    path("<int:pk>/edit", UpdateListView.as_view(), name="article_edit"),
+    path("<int:pk>", DetailListView.as_view, name="article_detail"),
+    path("<int:pk>/delete/", DeleteListView.as_view(), name="article_delete"),
+    path("", ArticleListView.as_view(), name="article_list")
+]
