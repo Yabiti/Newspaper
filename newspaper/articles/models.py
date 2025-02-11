44cae6519359ba92b046e6adc0c40cf2fd7cfc51
Yabiti <yabetseyohannes@gmail.com>
2025-02-11 02:26:15 -0800

    .

--- newspaper/articles/models.py
+++ newspaper/articles/models.py
@@ -18,4 +18,4 @@ class Article(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('article_detail', args=[str(self.id)])
+        return reverse('detail', args=[str(self.id)])
--- newspaper/articles/urls.py
+++ newspaper/articles/urls.py
@@ -10,7 +10,7 @@ from .views import( ArticleListView,
 
 urlpatterns = [
     path("<int:pk>/edit/", UpdateListView.as_view(), name="edit"),
-    path("<int:pk>/", DetailListView.as_view(), name="cleardetail"),
+    path("<int:pk>/", DetailListView.as_view(), name="detail"),
     path("<int:pk>/delete/", DeleteListView.as_view(), name="delete"),
     path("", ArticleListView.as_view(), name="list")
 ]
