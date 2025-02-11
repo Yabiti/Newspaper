04830f01cdbdf849276935cae52778f81b065159
Yabiti <yabetseyohannes@gmail.com>
2025-02-11 04:45:29 -0800

    .

--- newspaper/articles/urls.py
+++ newspaper/articles/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("<int:pk>/edit/", UpdateListView.as_view(), name="edit"),
     path("<int:pk>/", DetailListView.as_view(), name="detail"),
     path("<int:pk>/delete/", DeleteListView.as_view(), name="delete"),
-    path("", ArticleListView.as_view(), name="list")
+    path("list", ArticleListView.as_view(), name="list")
 ]
 
 if settings.DEBUG:
