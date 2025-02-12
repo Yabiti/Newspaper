5da331a0c6dd3a05e4757f0b950b867b4a34bae2
Yabiti <yabetseyohannes@gmail.com>
2025-02-12 07:50:57 -0800

    .

--- newspaper/articles/models.py
+++ newspaper/articles/models.py
@@ -23,14 +23,3 @@ class Article(models.Model):
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    comment = models.CharField(max_length=300)
-    author = models.ForeignKey(
-        get_user_model(),
-        on_delete=models.CASCADE
-    )
-
-    def __str__(self):
-        return self.comment
-    
-    def get_absolute_url(self):
-        return reverse('article_list')
