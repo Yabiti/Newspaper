d32b19314912df6590ae1e72dfae4881643f3609
Yabiti <yabetseyohannes@gmail.com>
2025-02-12 07:55:36 -0800

    .

--- newspaper/articles/models.py
+++ newspaper/articles/models.py
@@ -28,3 +28,10 @@ class Comment(models.Model):
         get_user_model(),
         on_delete=models.CASCADE
     )
+
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('article_list')
