106175ddca4f909caab3c4708484da95891f23f7
Yabiti <yabetseyohannes@gmail.com>
2025-02-13 01:40:47 -0800

    .

--- newspaper/articles/models.py
+++ newspaper/articles/models.py
@@ -22,12 +22,12 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,  related_name="comments")
     comment = models.CharField(max_length=300)
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
-        related_name="comments"
+       
     )
 
     
