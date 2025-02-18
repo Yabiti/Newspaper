1477f455a7d1cc62849a3da69a5e8c4fbee6ac1e
Yabiti <yabetseyohannes@gmail.com>
2025-02-18 02:12:41 -0800

    .

--- newspaper/articles/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from .models import Article
-class PostModelTest(TestCase):
-    def setUp(self):
-        Article.objects.create(text='just a test')
-    def test_text_content(self):
-        post=Article.objects.get(id=1)
-        expected_object_name = f'{post.text}'
-        self.assertEqual(expected_object_name, 'just a test')
