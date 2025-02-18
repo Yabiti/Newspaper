2fa498ffcde0b2378e598cc220ea09703c61e33b
Yabiti <yabetseyohannes@gmail.com>
2025-02-18 01:10:33 -0800

    .

--- newspaper/articles/tests.py
+++ newspaper/articles/tests.py
@@ -1,4 +1,6 @@
 from django.test import SimpleTestCase, TestCase
 from .models import Article
 # Create your tests here.
-class ModelTestCase(TestCase)
+class ArticleModelTestCase(TestCase):
+    def setUp(self):
+        return super().setUp()
