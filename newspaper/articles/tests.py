0a803a009dc91f34fbc4d9ce2ea1a6a61e46b139
Yabiti <yabetseyohannes@gmail.com>
2025-02-18 02:17:06 -0800

    .

--- newspaper/articles/tests.py
+++ newspaper/articles/tests.py
@@ -5,3 +5,5 @@ from .models import Article
 class HomePageViewTest(TestCase):
     def setUp(self):
         Article.objects.create(text="another test")
+    
+    def test_view_url_exist_at_proper_location(self):
