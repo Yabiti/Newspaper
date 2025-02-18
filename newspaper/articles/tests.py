929c15e739609193434df77db06e09990eade326
Yabiti <yabetseyohannes@gmail.com>
2025-02-18 02:23:12 -0800

    .

--- newspaper/articles/tests.py
+++ newspaper/articles/tests.py
@@ -13,3 +13,8 @@ class HomePageViewTest(TestCase):
     def test_view_url_by_name(self):
         resp = self.client.get(reverse('home'))
         self.assertEqual(resp.status_code, 200)
+    
+    def test_view_uses_correct_template(self):
+        resp = self.client.get("/")
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
