83d63e70580c26de0faf0b73f53bfd4ac31c898b
Yabiti <yabetseyohannes@gmail.com>
2025-02-18 02:05:46 -0800

    .

--- newspaper/articles/tests.py
+++ newspaper/articles/tests.py
@@ -5,3 +5,8 @@ from .models import Article
 class ArticleModelTest(TestCase):
     def setUp(self):
         Article.objects.create(text="just a test")
+    
+    def test_text_context(self):
+        post = Article.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'just a test')
